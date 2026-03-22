39511e0150326c33df816a0f4c7aad62961c052b
MOHD_ARSATH_26 <167057703+mohamed-arsath-001@users.noreply.github.com>
2026-03-22 08:54:51 +0530

    Delete test_data4.py

--- test_data4.py
+++
@@ -1,9 +0,0 @@
-from logic_engine import calculate_kpis
-from data_fetcher import fetch_dashboard_data
-
-df = fetch_dashboard_data()
-res = calculate_kpis(df)
-c2 = res.get("Cohort 2", {})
-reach = c2.get('reach', {})
-for k, v in reach.items():
-    print(f"{k}: {v}")
